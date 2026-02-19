271d1496127817cd70a75a5261b6c11fdb8dc7c3
Victorija Kashurkina <vskashurkina_1@edu.hse.ru>
2026-02-19 22:01:14 +0300

    :)

---
+++ user_sirvice/cmd.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"gitlab.education.tbank.ru/v.kashurkina-37455/user_service/internal"
+)
+
+func main() {
+    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
+    http.HandleFunc("/users", internal.GetUser)
+    http.ListenAndServe(":8080", nil)
+}
